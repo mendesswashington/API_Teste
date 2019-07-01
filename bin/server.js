805bf158feb4d085409e83675140b38a44095e67
mendesswashington <mendesswashington@gmail.com>
2019-07-01 09:56:19 -0300

    O código do servidor foi sepparado do código da aplicação.

---
+++ bin/server.js
@@ -0,0 +1,17 @@
+const app = require('../src/app');
+const express = require('express');
+const http = require('http');
+
+
+const port = 3000;
+const hostname = 'localhost';
+
+const server = http.createServer(app);
+
+
+
+
+//Inicia o servidor npm start
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
